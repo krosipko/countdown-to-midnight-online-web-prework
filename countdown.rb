b9dbb827a08e4492fbda22e82bd945271b92f0a7
Kyle Rosipko <krosipko@outlook.com>
2019-01-11 04:47:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
-
-def countdown
+def countdown(count)
+  x = count 
+  while x != 0 
+    x = x - 1
+    puts "#{x} SECOND(S)!"
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(count)
+  x = count 
+  while x != 0 
+    x = x - 1
+    puts "#{x} SECOND(S)!"
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end 
